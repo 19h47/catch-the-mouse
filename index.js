7bddbb4b85d1de6e47fb25854d8a8bafd7a13163
Jérémy Levron <levronjeremy@19h47.fr>
2017-09-13 17:37:19 +0200

    Update script (WIP)

--- index.js
+++ index.js
@@ -1,35 +1,67 @@
 let informations_element = document.querySelector('.js-informations');
 let boxInformations = {};
 
+
 const box = document.querySelector('.js-box');
+let BoxRect = box.getBoundingClientRect();
 
-// Update informations
-update();
-window.onresize = update;
 
-document.body.onmousemove = function(event) {
+document.body.onmousemove = (event) => {
 
-	boxInformations = {
-		clientX: event.clientX,
-		clientY: event.clientY
-	};
+	// boxInformations = {
+	// 	clientX: event.clientX,
+	// 	clientY: event.clientY
+	// };
 
 	update();
 };
+
+
+box.onmousemove = (event, element) => {
+	// console.log(BoxRect);
+	boxInformations = {
+		BoxClientX: event.clientX - BoxRect.left,
+		BoxClientY: event.clientY - BoxRect.top,
+		BoxClientXMiddle: ( event.clientX - BoxRect.left ) - ( box.clientWidth / 2 ),	
+		BoxClientYMiddle: ( event.clientY - BoxRect.top ) - ( box.clientHeight / 2 )	
+	};
+
+	if ( boxInformations.BoxClientYMiddle > 0 ) {
+		box.style.transform = "translateY(" + ( boxInformations.BoxClientYMiddle / 10 ) + "px)";
+	}
+
+	if ( boxInformations.BoxClientYMiddle < 0 ) {
+		box.style.transform = "translateY(" + ( boxInformations.BoxClientYMiddle / -10 ) + "px)";
+	}
+
+	if ( boxInformations.BoxClientXMiddle > 0 ) {
+		box.style.transform = "translateX(" + ( boxInformations.BoxClientXMiddle / 10 ) + "px)";
+	}
+
+	if ( boxInformations.BoxClientXMiddle < 0 ) {
+		box.style.transform = "translateX(" + ( boxInformations.BoxClientXMiddle / -10 ) + "px)";
+	}
+};
+
+box.onmouseout = () => {
+	box.style.transform = '';
+}
 
 
 /**
  * Update
  */
 function update() {
-	// First clear infomrations box
+	BoxRect = box.getBoundingClientRect();
+
+	// First clear information box
 	informations_element.innerHTML = '';
 
 	Object.assign(boxInformations, {
 		clientHeight: `${box.clientHeight}px`,
 		clientWidth: `${box.clientWidth}px`,
 		offsetTop: `${box.offsetTop}px`,
-		offsetLeft: `${box.offsetLeft}px` 
+		offsetLeft: `${box.offsetLeft}px`	
 	});
 
 	for ( var key in boxInformations) {
@@ -41,4 +73,14 @@ function update() {
 		
 		informations_element.appendChild(node);
 	}
+};
+
+
+/**
+ * Onload
+ */
+window.onload = function() {
+	// Update informations
+	update();
+	window.onresize = update;
 };
